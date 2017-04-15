b37b71acb4d58b6225242de1be8688b6b8592ca4
Anmol Desai <i-anmol@edmodo.com>
2017-04-14 23:34:12 -0700

    set up links

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render(){
+    return (
+      <div>
+        Countdown
+      </div>
+    )
+  }
+});
+
+module.exports = Countdown;
