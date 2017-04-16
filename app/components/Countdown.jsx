9e0c8c346a2d212a67be21cc31efeba2ee7b4e88
Anmol Desai <i-anmol@edmodo.com>
2017-04-16 11:09:12 -0700

    tests for countdown using spy and async tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -6,14 +6,39 @@ var Countdown = React.createClass({
 
   getInitialState(){
     return {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
+    }
+  },
+
+  startTimer(){
+    // allows acccess to timer through the object
+    this.timer = setInterval(() =>{
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    },1000);
+  },
+
+  // after props or state is updated
+  componentDidUpdate(prevProps,prevState){
+    if(this.countdownStatus !== prevState.countdownStatus){
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        default:
+
+      }
     }
   },
 
   handleSetCountdown(seconds){
     this.setState({
-      count:seconds
-    })
+      count:seconds,
+      countdownStatus: 'started'
+    }); // calls component did update
   },
 
   render(){
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,47 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+// test modules from react
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  it('should exist',() => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handle set countdown', () => {
+    // done lets mocha know that the test is aysnc and it should wait till done before stopping the test
+    it('should set state to started and count down', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(10); // call internal component method
+
+      // countdown status must be updated
+      expect(countdown.state.countdownStatus).toBe('started');
+      // count should be updated
+      expect(countdown.state.count).toBe(10);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      },1001);
+    });
+
+    it('should not set count below 0', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(1); // call internal component method
+
+      // countdown status must be updated
+      expect(countdown.state.countdownStatus).toBe('started');
+      // count should be updated
+      expect(countdown.state.count).toBe(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      },3001);
+    });
+  });
+});
