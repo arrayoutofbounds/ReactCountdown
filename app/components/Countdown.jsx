9fb7f68b3e79c1a66b7e9494505b2d28e0d2cfb5
Anmol Desai <i-anmol@edmodo.com>
2017-04-17 09:35:27 -0700

    countdown done

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -13,14 +13,38 @@ var Countdown = React.createClass({
   },
 
   startTimer(){
-    console.log("Start timer called");
     // allows acccess to timer through the object
     this.timer = setInterval(() =>{
       var newCount = this.state.count - 1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+      // countdown is complete
+      if(newCount === 0){
+        this.setState({countdownStatus: 'stopped'});
+      }
     },1000);
+  },
+
+  /* just before the component is shown to the screen via render. No access to refs or dom as not shown yet
+  componentWillMount(){
+    console.log("component will mount");
+  },
+
+  // right after everything is rendered in dom. Access to refs etc
+  componentDidMount(){
+    console.log("component did mount");
+  },
+
+  componentWillUpdate(nextProps,nextState){
+
+  },
+  */
+
+  // fired before component is removed from dom visually.
+  componentWillUnmount(){
+    clearInterval(this.timer); // stop interval
+    this.timer = undefined; // clears the var
   },
 
   // after props or state is updated
@@ -67,6 +91,7 @@ var Countdown = React.createClass({
     };
     return (
       <div>
+        <h1 className="page-title">Countdown App</h1>
         <Clock totalSeconds={count} />
         {renderControlArea()}
       </div>
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -43,5 +43,31 @@ describe('Countdown', () => {
         done();
       },3001);
     });
+
+    it('should pause countdown on pause status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3); // call internal component method to start countdown
+
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      },1001);
+    });
+
+    it('should stop countdown on stop status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3); // call internal component method to start countdown
+
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      },1001);
+    });
   });
 });
